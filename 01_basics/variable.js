b0b79d2440bdf2d06b96f16615f5d4cf57c3b13f
Akash R <akashrchinnu@gmail.com>
2024-09-18 14:45:39 +0000

    variables done

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const yourAge = 21;
+let yourName = "Akash";
+var yourEmail = "akash@gmail.com";
+yourNumber = "1542126351";
+let yourFutcher;
+
+
+console.log(yourName);
+
+console.table([yourAge,yourName,yourEmail,yourNumber,yourFutcher]);
