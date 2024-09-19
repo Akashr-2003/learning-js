2c63a37c0c5b47796cdbe15e5184ad3afc098ea4
Akash R <akashrchinnu@gmail.com>
2024-09-19 04:56:23 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+let yourAge = 21;
+let yourName = "Akash";
+let yourEmail = "akash@gmail.com";
+let isLoggedIn = true;
+let yourFutcher;
+
+
+
+console.table({
+    yourAge: typeof yourAge,
+    yourName: typeof yourName,
+    yourEmail: typeof yourEmail,
+    isLoggedIn: typeof isLoggedIn,
+    yourFutcher: typeof yourFutcher
+  });
